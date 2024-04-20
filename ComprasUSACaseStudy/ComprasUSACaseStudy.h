bd858f12501733408e199b6f0afdfc2161538fc5
Marcos Apostolo <marcos.vi.am.ap@gmail.com>
2024-04-20 13:09:59 -0300

    Initial commit

---
+++ ComprasUSACaseStudy/ComprasUSACaseStudy.h
@@ -0,0 +1,18 @@
+//
+//  ComprasUSACaseStudy.h
+//  ComprasUSACaseStudy
+//
+//  Created by Marcos Amaral on 20/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ComprasUSACaseStudy.
+FOUNDATION_EXPORT double ComprasUSACaseStudyVersionNumber;
+
+//! Project version string for ComprasUSACaseStudy.
+FOUNDATION_EXPORT const unsigned char ComprasUSACaseStudyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ComprasUSACaseStudy/PublicHeader.h>
+
+

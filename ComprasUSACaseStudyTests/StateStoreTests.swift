125f0258dc5ad5f2e74b0860a47d04929931461a
Marcos Apostolo <marcos.vi.am.ap@gmail.com>
2024-04-20 17:06:17 -0300

    CoreDataStateStore delivers empty states twice (no side effects)

--- ComprasUSACaseStudyTests/StateStoreTests.swift
+++ ComprasUSACaseStudyTests/StateStoreTests.swift
@@ -33,4 +33,44 @@ final class StateStoreTests: XCTestCase {
         
         wait(for: [exp], timeout: 1.0)
     }
+    
+    func test_retrieve_hasNoSideEffect_afterReturningEmpty() {
+        let sut = CoreDataStateStore()
+                
+        let exp = expectation(description: "Wait for cache retrieval")
+        let exp2 = expectation(description: "Wai for second retrieval")
+        
+        sut.retrieve { receivedResult in
+            switch receivedResult {
+            case let .success(states):
+                XCTAssertEqual(states, [])
+            case let .failure(error):
+                XCTFail("Expected success but got \(error) instead")
+            }
+            
+            exp.fulfill()
+        }
+        
+        sut.retrieve { receivedResult in
+            switch receivedResult {
+            case let .success(states):
+                XCTAssertEqual(states, [])
+            case let .failure(error):
+                XCTFail("Expected success but got \(error) instead")
+            }
+            
+            exp2.fulfill()
+        }
+        
+        wait(for: [exp, exp2], timeout: 1.0)
+    }
+    
+    // Helpers
+    func makeSUT() -> CoreDataStateStore {
+        let sut = CoreDataStateStore()
+        
+        checkForMemoryLeaks(sut)
+        
+        return sut
+    }
 }

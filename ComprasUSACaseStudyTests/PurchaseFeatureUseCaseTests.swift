65d97834be76b14962ec2fba358867c2dd1cf3bf
Marcos Apostolo <marcos.vi.am.ap@gmail.com>
2024-04-23 20:55:53 -0300

    PurchaseFeatureUseCase does not message store on init

--- ComprasUSACaseStudyTests/PurchaseFeatureUseCaseTests.swift
+++ ComprasUSACaseStudyTests/PurchaseFeatureUseCaseTests.swift
@@ -9,5 +9,48 @@ import XCTest
 import ComprasUSACaseStudy
 
 final class PurchaseFeatureUseCaseTests: XCTestCase {
+    func test_init_doesNotMessageStore() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.retrievalCompletions.count, 0)
+        XCTAssertEqual(store.insertionCompletions.count, 0)
+        XCTAssertEqual(store.deletionCompletions.count, 0)
+        XCTAssertEqual(store.editionCompletions.count, 0)
+    }
     
+    // MARK: Helpers
+    func makeSUT() -> (sut: PurchaseFeatureUseCase, store: PurchaseStoreSpy) {
+        let store = PurchaseStoreSpy()
+        let sut = PurchaseFeatureUseCase(store: store)
+        
+        checkForMemoryLeaks(store)
+        checkForMemoryLeaks(sut)
+        
+        return (sut, store)
+    }
+}
+
+class PurchaseStoreSpy: PurchaseStore {
+    var retrievalCompletions = [RetrievalCompletion]()
+    var insertionCompletions = [InsertionCompletion]()
+    var deletionCompletions = [DeletionCompletion]()
+    var editionCompletions = [EditionCompletion]()
+}
+
+extension PurchaseStoreSpy {
+    func retrievePurchases(completion: @escaping RetrievalCompletion) {
+        
+    }
+    
+    func insert(_ purchase: ComprasUSACaseStudy.LocalPurchase, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    func delete(_ purchase: ComprasUSACaseStudy.LocalPurchase, completion: @escaping DeletionCompletion) {
+        
+    }
+    
+    func edit(_ purchase: ComprasUSACaseStudy.LocalPurchase, completion: @escaping EditionCompletion) {
+        
+    }
 }

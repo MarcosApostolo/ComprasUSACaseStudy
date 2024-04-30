c8bd8f404450e3625f797548c94fb2dd220b771e
Marcos Apostolo <marcos.vi.am.ap@gmail.com>
2024-04-30 08:41:38 -0300

    Configure initial scene

--- ComprasUSACaseStudyApp/ComprasUSACaseStudyApp/SceneDelegate.swift
+++ ComprasUSACaseStudyApp/ComprasUSACaseStudyApp/SceneDelegate.swift
@@ -6,6 +6,10 @@
 //
 
 import UIKit
+import Combine
+import CoreData
+import ComprasUSAiOS
+import ComprasUSACaseStudy
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -16,7 +20,28 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        // This force try will be removed once the App composition is complete
+        let store = try! CoreDataStore(storeURL: NSPersistentContainer
+            .defaultDirectoryURL()
+            .appendingPathComponent("feed-store.sqlite"))
+        
+        let loader = PurchaseFeatureUseCase(store: store)
+        
+        let vc = PurchasesListUIComposer.composePurchasesList(loader: {
+            loader
+                .loadPurchasesPublisher()
+                .eraseToAnyPublisher()
+        }, onPurchaseRegister: {})
+        
+        let window = UIWindow(windowScene: scene)
+        
+        window.makeKeyAndVisible()
+        
+        window.rootViewController = vc
+        
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {

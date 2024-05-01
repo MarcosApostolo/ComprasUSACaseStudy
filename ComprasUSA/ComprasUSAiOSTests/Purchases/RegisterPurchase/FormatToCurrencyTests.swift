c4edcf2aba0ff265512b4db8c8cf1ce711aa0e83
Marcos Apostolo <marcos.vi.am.ap@gmail.com>
2024-05-01 18:39:44 -0300

    FormatToCurrency returns formatted values when receiving valid text

--- ComprasUSA/ComprasUSAiOSTests/Purchases/RegisterPurchase/FormatToCurrencyTests.swift
+++ ComprasUSA/ComprasUSAiOSTests/Purchases/RegisterPurchase/FormatToCurrencyTests.swift
@@ -8,8 +8,32 @@
 import Foundation
 import XCTest
 
-func formatToCurrency(from text: String) -> String {
-    return ""
+func formatToCurrency(from text: String, locale: Locale = .init(identifier: "en-US")) -> String {
+    let formatter = NumberFormatter()
+    formatter.locale = locale
+    formatter.numberStyle = .currencyAccounting
+    formatter.maximumFractionDigits = 2
+    formatter.minimumFractionDigits = 2
+    
+    guard let regex = try? NSRegularExpression(pattern: "[^0-9]", options: .caseInsensitive) else {
+        return ""
+    }
+    let cleanText = regex.stringByReplacingMatches(
+        in: text,
+        options: NSRegularExpression.MatchingOptions(rawValue: 0),
+        range: NSMakeRange(0, text.count),
+        withTemplate: ""
+    )
+    
+    let double = (cleanText as NSString).doubleValue
+    
+    let number = NSNumber(value: (double / 100))
+    
+    guard number != 0, let formattedNumber = formatter.string(from: number) else {
+        return ""
+    }
+    
+    return formattedNumber
 }
 
 final class FormatToCurrencyTests: XCTestCase {
@@ -23,5 +47,13 @@ final class FormatToCurrencyTests: XCTestCase {
         let value = formatToCurrency(from: "?!@#_+ˆ&%(")
         
         XCTAssertEqual(value, "")
+    }
+    
+    func test_returnsFormattedValue_whenReceivingValidNumber() {
+        XCTAssertEqual(formatToCurrency(from: "1"), "$0.01")
+        XCTAssertEqual(formatToCurrency(from: "$0.12"), "$0.12")
+        XCTAssertEqual(formatToCurrency(from: "$1.12"), "$1.12")
+        XCTAssertEqual(formatToCurrency(from: "$1.122"), "$11.22")
+        XCTAssertEqual(formatToCurrency(from: "$1.1223"), "$112.23")
     }
 }

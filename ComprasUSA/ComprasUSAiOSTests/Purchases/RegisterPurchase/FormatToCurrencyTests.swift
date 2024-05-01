7da8d13b41b0f4696dfdb1a4491145229f44bbc3
Marcos Apostolo <marcos.vi.am.ap@gmail.com>
2024-05-01 18:26:31 -0300

    FormatToCurrency returns empty when receiving empty

---
+++ ComprasUSA/ComprasUSAiOSTests/Purchases/RegisterPurchase/FormatToCurrencyTests.swift
@@ -0,0 +1,21 @@
+//
+//  FormatToCurrencyTests.swift
+//  ComprasUSAiOSTests
+//
+//  Created by Marcos Amaral on 01/05/24.
+//
+
+import Foundation
+import XCTest
+
+func formatToCurrency(from text: String) -> String {
+    return ""
+}
+
+final class FormatToCurrencyTests: XCTestCase {
+    func test_returnsEmpty_whenEmptyText() {
+        let value = formatToCurrency(from: "")
+        
+        XCTAssertTrue(value.isEmpty)
+    }
+}
